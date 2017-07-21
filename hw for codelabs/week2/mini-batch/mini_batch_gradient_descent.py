cb485c5cb106a5dfb14e39f7415828a05e258dbc
yangjiqi <yang.jiq@husky.neu.edu>
2017-07-21 14:51:20 -0700

    added adagrad

--- hw for codelabs/week2/mini-batch/mini_batch_gradient_descent.py
+++ hw for codelabs/week2/mini-batch/mini_batch_gradient_descent.py
@@ -5,6 +5,8 @@ from sklearn.preprocessing import MinMaxScaler
 import sklearn.linear_model as linear_model
 import sklearn.metrics as metrics
 import sklearn
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 
 def data_preprocessing(shuffle_data = False):
     # read source data from csv file
@@ -63,12 +65,15 @@ def logistic_grad_func(theta, x, y):
 
     return grad
 
-def mini_batch_gradient_descent(theta, X_train, y_train, lr=0.00001, epochs=1000,
-                                  batch_size=50,
+def mini_batch_gradient_descent(theta, X_train, y_train, lr=0.005, epochs=1000,
+                                  batch_size=10,
                                   momentum_flag = False,
                                   momentum=0.9,
-                                  epsilon=1e-20,
+                                  epsilon=1e-10,
+                                  converge_change=1e-3,
                                   verbose = False,
+                                  adam_eps = 0,
+                                  adamgrad_flag = False
                                   ):
     m = len(X_train)
     error = np.zeros(theta.shape[1])
@@ -76,19 +81,30 @@ def mini_batch_gradient_descent(theta, X_train, y_train, lr=0.00001, epochs=1000
     error_list = []
     cost = logistic_cost_func(theta, X_train, y_train)
     error_list.append(cost)
+    grad_sum = 0
 
     for epoch in range(epochs):
+        prev_cost = cost
         prev_theta = 0
         for batch_i in range(m // batch_size):
             batch_X = X_train[batch_i * batch_size:(batch_i + 1) * batch_size]
             batch_y = y_train[batch_i * batch_size:(batch_i + 1) * batch_size]
             # print(batch_i + 1)
             grad = logistic_grad_func(theta, batch_X, batch_y)
+
+
+
             if momentum_flag:
                 theta -= (lr * (grad * 1.0 / batch_size) - momentum * prev_theta)
                 prev_theta = lr * (grad * 1.0 / batch_size) - momentum * prev_theta
+            elif adamgrad_flag:
+                # for adagrad
+                grad_sum += grad ** 2
+                theta -= lr * grad * 1.0 / np.sqrt(adam_eps + grad_sum)
             else:
+                # naive mini-batch
                 theta -= lr * (grad * 1.0 / batch_size)
+
 
             # print("batch cost ", logistic_cost_func(theta, batch_X, batch_y))
 
@@ -97,11 +113,17 @@ def mini_batch_gradient_descent(theta, X_train, y_train, lr=0.00001, epochs=1000
         if verbose:
             print ("epoch:{0} cost:{1}".format(epoch, error_list[-1]))
 
-        if np.linalg.norm(theta - error) < epsilon:
-            break
-        else:
-            error = theta
+        # if np.linalg.norm(theta - error) < epsilon:
+        #     print("epochs in total : {}".format(epoch))
+        #     break
+        # else:
+        #     error = theta
 
+        cost_change = abs(cost - prev_cost)
+        if cost_change < converge_change:
+            print("epochs in total : {}".format(epoch))
+            break
+        X_train, y_train = shuffle(X_train, y_train)
     return theta, error_list
 
 
@@ -136,12 +158,25 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = split_data(X, y)
 
     theta1 = np.random.rand(1, X_train.shape[1] + 1)
-    theta1, error_list = mini_batch_gradient_descent(theta1, X_train, y_train)
+    theta1, error_list1 = mini_batch_gradient_descent(theta1, X_train, y_train)
     # cm1, f1_1 = f1_score(X_train, X_test, y_train, y_test)
     print("Accuracy of mini-batch : {}".format(accuracy(theta1, X_test, y_test)))
     # print("metrics:\n {}".format(cm1))
     # print("f1 score: {}".format(f1_1))
 
     theta2 = np.random.rand(1, X_train.shape[1] + 1)
-    theta2, error_list = mini_batch_gradient_descent(theta2, X_train, y_train, momentum_flag=True)
+    theta2, error_list2 = mini_batch_gradient_descent(theta2, X_train, y_train, momentum_flag=True)
     print("Accuracy of mini-batch with momentum(0.9) : {}".format(accuracy(theta2, X_test, y_test)))
+
+    theta3 = np.random.rand(1, X_train.shape[1] + 1)
+    theta3, error_list3 = mini_batch_gradient_descent(theta3, X_train, y_train, adamgrad_flag=True)
+    # cm1, f1_1 = f1_score(X_train, X_test, y_train, y_test)
+    print("Accuracy of mini-batch with adagrad : {}".format(accuracy(theta3, X_test, y_test)))
+
+
+    plt.plot(range(len(error_list1[0:])), error_list1[0:], color='red', label='Mini Batch SGD')
+    plt.plot(range(len(error_list2[0:])), error_list2[0:], color='yellow', label='Mini Batch SGD with Momentum')
+    plt.plot(range(len(error_list3[0:])), error_list3[0:], color='green',
+             label='Mini Batch SDG with Adagrad')
+    plt.legend(bbox_to_anchor=(1., 1.), loc=0, borderaxespad=0.)
+    plt.show()
